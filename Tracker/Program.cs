c0c938d873f99ea10ce90a0955fce710317efc4e
Ришат Каримов <karimr2000@mail.ru>
2024-04-16 03:20:30 +0300

    Add project files.

---
+++ Tracker/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using Tracker.Models;
+
+namespace Tracker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense
+                ("Mgo+DSMBMAY9C3t2UFhhQlJBfVpdXGZWfFN0QXNcdV9yflFPcDwsT3RfQFljTX9Wd0JiX35feXBUTg==");
+
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
