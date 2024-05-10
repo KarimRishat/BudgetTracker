d4f5f19023561132281de05d4c371300518bade6
Ришат Каримов <karimr2000@mail.ru>
2024-05-10 23:21:12 +0300

    LoginResponse

---
+++ Tracker/Models/LoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Tracker.Models
+{
+    public class LoginResponse
+    {
+        public LocalUser User { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ Tracker/Repository/UserRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Tracker.Models;
+
+namespace Tracker.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private string secretKey;
+        public UserRepository(ApplicationDbContext db, IConfiguration configuration)
+        {
+            _db = db;
+            secretKey = configuration.GetValue<string>("Apisettings:Secret");
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+            if (user == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<LoginResponse> Login(LoginRequest loginRequest)
+        {
+            var user = _db.LocalUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequest.UserName.ToLower()
+            && u.Password == loginRequest.Password);
+            if (user == null)
+            {
+                return null;
+            }
+
+            //если пользователя нашли генерируется токен jwt
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.UserId.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            LoginResponse loginResponse = new LoginResponse()
+            {
+                Token = tokenHandler.WriteToken(token),
+                User = user
+            };
+            return loginResponse;
+        }
+
+        public async Task<LocalUser> Register(RegistrationRequest registrationRequest)
+        {
+            LocalUser user = new LocalUser()
+            {
+                UserName = registrationRequest.UserName,
+                Password = registrationRequest.Password,
+                Name = registrationRequest.Name,
+                Role = registrationRequest.Role,
+            };
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
+        }
+    }
+}
